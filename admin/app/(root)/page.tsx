113c56f828bea4dd9c4b7280bac82ef456226d53
shovoncse <shovondas26@gmail.com>
2023-10-25 22:05:15 +0300

    main functionality of auth added

---
+++ admin/app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function SetupPage() {
+ return (
+   <div>
+     This is a protected page. You must be signed in to access it.
+   </div>
+ )
+}
