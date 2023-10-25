e9552082e501f49f0941cc9e51174e93fda1e696
shovoncse <shovondas26@gmail.com>
2023-10-25 22:13:09 +0300

    authentication added

--- admin/app/(root)/page.tsx
+++ admin/app/(root)/page.tsx
@@ -1,8 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function SetupPage() {
  return (
    <div>
+    <UserButton afterSignOutUrl="/" />
      This is a protected page. You must be signed in to access it.
    </div>
  )
