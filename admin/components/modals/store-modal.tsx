b59fd0e0008b04092bece063fad59a1cfc8f60de
shovoncse <shovondas26@gmail.com>
2023-11-04 14:55:34 +0200

    store redirect logic

--- admin/components/modals/store-modal.tsx
+++ admin/components/modals/store-modal.tsx
@@ -38,7 +38,7 @@ export const StoreModal = () => {
     try {
       setLoading(true);
       const response = await axios.post('/api/stores', data)
-      toast.success("Store created.")
+      window.location.assign(`/${response.data.id}`);
     } catch (error) {
       toast.error("Something went wrong.")
     } finally {
