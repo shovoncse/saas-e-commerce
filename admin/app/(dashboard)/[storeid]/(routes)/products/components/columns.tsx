6961cbc726cd0acc621a8f22c6735dc55007fcde
shovoncse <shovondas26@gmail.com>
2023-11-15 23:59:48 +0200

    :sparkles: product module full featured added

---
+++ admin/app/(dashboard)/[storeid]/(routes)/products/components/columns.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+
+import { CellAction } from "./cell-action";
+
+export type ProductColumn = {
+  id: string;
+  name: string;
+  isFeatured: boolean;
+  isArchived: boolean;
+  price: string;
+  category: string;
+  color: string;
+  size: string;
+  createdAt: string;
+};
+
+export const columns: ColumnDef<ProductColumn>[] = [
+  {
+    accessorKey: "name",
+    header: "Name",
+  },
+  {
+    accessorKey: "isFeatured",
+    header: "Featured",
+  },
+  {
+    accessorKey: "isArchived",
+    header: "Archived",
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "category",
+    header: "Category",
+  },
+  {
+    accessorKey: "color",
+    header: "Color",
+    cell: ({ row }) => (
+      <div className="flex items-center gap-x-2">
+        {row.original.color}
+        <div
+          className="h-6 w-6 rounded-full border"
+          style={{ backgroundColor: row.original.color }}
+        />
+      </div>
+    ),
+  },
+  {
+    accessorKey: "size",
+    header: "Size",
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Date",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => <CellAction data={row.original} />,
+  },
+];
