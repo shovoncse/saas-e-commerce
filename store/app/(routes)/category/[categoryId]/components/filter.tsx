661f7e88ba786fb8d1bc437686c67a317e0808cd
shovoncse <shovondas26@gmail.com>
2023-11-21 23:56:18 +0200

    :construction: category page filters

---
+++ store/app/(routes)/category/[categoryId]/components/filter.tsx
@@ -0,0 +1,58 @@
+import qs from "query-string";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import { Size, Color } from "@/types";
+import Button from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+interface FilterProps {
+  data: (Size | Color)[];
+  valueKey: string;
+  name: string;
+}
+
+const Filter: React.FC<FilterProps> = ({ data, valueKey, name }) => {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const selectedValue = searchParams.get(valueKey);
+
+  const onClick = (id: string) => {
+    const current = qs.parse(searchParams.toString());
+    const query = {
+      ...current,
+      [valueKey]: id,
+    };
+
+    if (current[valueKey] === id) {
+      query[valueKey] = null;
+    }
+
+    const url = qs.stringifyUrl(
+      {
+        url: window.location.href,
+        query,
+      },
+      { skipNull: true }
+    );
+
+    router.push(url);
+  };
+  return (
+    <div className="mt-8">
+      <h3 className="text-lg font-semibold">{name}</h3>
+      <hr className="my-4" />
+      <div className="flex flex-wrap gap-2">
+        {data.map((filter) => (
+          <div key={filter.id} className="flex items-center">
+            <Button onClick={() => onClick(filter.id)} className={cn("text-sm rounded-md text-gray-800 p-2 bg-white border border-gray-300", selectedValue === filter.id && "bg-black text-white" )}>
+              {filter.name}
+            </Button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
