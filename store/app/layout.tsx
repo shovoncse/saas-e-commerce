5942cd9e84daa923fd5a433b9d3122ceabece033
shovoncse <shovondas26@gmail.com>
2023-11-22 21:27:40 +0200

    :bug: toast provider was missing

--- store/app/layout.tsx
+++ store/app/layout.tsx
@@ -1,16 +1,17 @@
-import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
 
-import './globals.css'
-import Footer from '@/components/footer'
-import Navbar from '@/components/navbar'
 import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
+
+import './globals.css'
 
 const font = Urbanist({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
+export const metadata = {
   title: 'Store',
-  description: 'Store',
+  description: 'Store - The place for all your purchases.',
 }
 
 export default function RootLayout({
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToastProvider />
         <ModalProvider />
         <Navbar />
         {children}
