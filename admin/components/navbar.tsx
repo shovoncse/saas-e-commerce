8ea1d4c9e348783d390de89b7acd20c675dd7f89
shovoncse <shovondas26@gmail.com>
2023-11-23 16:50:11 +0200

    :sparkles: dark mode

--- admin/components/navbar.tsx
+++ admin/components/navbar.tsx
@@ -1,7 +1,9 @@
 import { UserButton, auth } from "@clerk/nextjs";
-import { MainNav } from "@/components/main-nav";
-import StoreSwitcher from "@/components/store-switcher";
 import { redirect } from "next/navigation";
+
+import StoreSwitcher from "@/components/store-switcher";
+import { MainNav } from "@/components/main-nav";
+import { ThemeToggle } from "@/components/theme-toggle";
 import prismadb from "@/lib/prismadb";
 
 const Navbar = async () => {
@@ -16,18 +18,19 @@ const Navbar = async () => {
       userId,
     }
   });
-  
-  return (
+
+  return ( 
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
-        <div className="ml-auto items-center space-x-4">
-          <UserButton />
+        <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
+          <UserButton afterSignOutUrl="/" />
         </div>
       </div>
     </div>
   );
-}
-
+};
+ 
 export default Navbar;
