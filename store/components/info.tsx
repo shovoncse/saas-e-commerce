5cec371fabf1532b5be7336b841325a79fae23c1
shovoncse <shovondas26@gmail.com>
2023-11-23 15:55:05 +0200

    :bug: fixed

--- store/components/info.tsx
+++ store/components/info.tsx
@@ -1,14 +1,24 @@
 "use client";
-import { Product } from "@/types";
-import Currency from "@/components/ui/currency";
-import Button from "@/components/ui/button";
+
 import { ShoppingCart } from "lucide-react";
 
+import Currency  from "@/components/ui/currency";
+import Button from "@/components/ui/button";
+import { Product } from "@/types";
+import useCart from "@/hooks/use-cart";
+
 interface InfoProps {
-  data: Product;
-}
+  data: Product
+};
+
 const Info: React.FC<InfoProps> = ({ data }) => {
-  return (
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  }
+
+  return ( 
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
       <div className="mt-3 flex items-end justify-between">
@@ -20,24 +30,23 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       <div className="flex flex-col gap-y-6">
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Size:</h3>
-          <div>{data?.size?.name}</div>
+          <div>
+            {data?.size?.value}
+          </div>
         </div>
         <div className="flex items-center gap-x-4">
           <h3 className="font-semibold text-black">Color:</h3>
-          <div
-            className="w-6 h-6 rounded-full border border-gray-600"
-            style={{ backgroundColor: data?.color?.value }}
-          />
+          <div className="h-6 w-6 rounded-full border border-gray-600" style={{ backgroundColor: data?.color?.value }} />
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2">
-          Add to Cart
-          <ShoppingCart />
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
+          Add To Cart
+          <ShoppingCart size={20} />
         </Button>
       </div>
     </div>
   );
-};
-
+}
+ 
 export default Info;
