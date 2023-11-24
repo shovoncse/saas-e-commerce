c194c90e7d069809a8d9e13b93d3adfa77647deb
shovoncse <shovondas26@gmail.com>
2023-11-24 22:21:41 +0200

    :bug: fix

--- admin/components/ui/image-upload.tsx
+++ admin/components/ui/image-upload.tsx
@@ -53,7 +53,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="qajv0lys">
         {({ open }) => {
           const onClick = () => {
             open();
