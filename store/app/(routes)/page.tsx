242cb9f6a0a9e594b963c07e61cadad71273385b
shovoncse <shovondas26@gmail.com>
2023-11-24 22:38:28 +0200

    :bug: fix

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("ccf18ef8-51a9-4935-9ab9-a09c13ef71a3");
 
   return (
     <Container>
